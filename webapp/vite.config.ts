b385baf91950c3b691208d825c5691fff1536f11
virtualvasu <virtualvasu624@gmail.com>
2025-06-12 23:48:55 +0530

    adding interactive UI

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   build: {
     target: 'es2020',
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
 });
 
 
